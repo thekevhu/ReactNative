be43c3270631d26d82f1a2b0bf474457cd47722e
Kevin Hu <kevinhu@Kevins-MacBook-Pro.local>
2018-05-26 22:11:06 -0400

    done

---
+++ navigation/main_tab_bar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createBottomTabNavigator } from 'react-navigation';
+// import { View, Text } from 'react-native';
+import Ionicons from 'react-native-vector-icons/FontAwesome';
+import About from '../components/about';
+import SearchTab from './search_tab';
+
+// const SearchTab = (props) => {
+//   return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Search</Text></View>;
+// };
+
+
+const MainTabBar = createBottomTabNavigator(
+  {
+    SearchTab,
+    AboutTab: {
+      screen: About,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: 'About',
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name="info-circle"
+            size={26}
+            color={focused ? '#58AADA' : 'grey'}
+          />
+        ),
+      }),
+    },
+  },
+  {
+    initialRouteName: 'SearchTab',
+  },
+);
+
+
+export default MainTabBar;
